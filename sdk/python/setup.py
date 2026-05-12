74b9427aab131964e34a757eb19f8f6cfd1eef4c
Mohi1038 <b23cs1038@iitj.ac.in>
2026-05-12 14:55:32 +0530

    chore: add license and update SDK metadata

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="memos-sdk",
     version="0.1.0",
     description="Python SDK for the Distributed Memory OS (MemOS)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
         "grpcio>=1.50.0",
